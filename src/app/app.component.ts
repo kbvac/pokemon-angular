fc9ab34c142076a0e11acf77722946b2fb73fa2d
kbvac <angechristian2001.ac@gmail.com>
2024-03-04 09:51:43 +0000

    adding mitigated option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,15 @@ import { Component, OnInit } from "@angular/core";
 export class AppComponent implements OnInit {
   ngOnInit(): void {
     console.log("la page a chargée...");
+
+    let deferredPrompt;
+    typeof window !== "undefined" && window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      console.log("vous pouvez installer l'application...");
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+    });
+
   }
 }
